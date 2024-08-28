243e24aacda0527ca6c0fd14bbfb626c40102dee
Zucarito <diego.campos5701@alumnos.udg.mx>
2024-08-28 16:41:11 -0600

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <p> Hola Mundo </p>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
